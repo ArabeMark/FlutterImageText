ab2afa100b2c43f9aad19944a9eb10ca63844b5d
Mark Benjie Arabe <markarabearabe@gmail.com>
2025-09-08 17:26:37 +0800

    Flutter Image Poster

--- main.dart
+++ main.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 
+/*The purpose of this syntax is to import flutter material design library
+import - Dart keyword to include external libraries
+package:flutter/ - material package that comes with flutter
+This is essential for building flutter apps with standard UI
+ *  */
+
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); // this function is to initialize the myApp that attached the root widget
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override //indicates this method overrides StatelessWidget build
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Activity 1',
+      title: 'Hello Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: const MyHomePage(),
-      theme: ThemeData(scaffoldBackgroundColor: Colors.cyan),
-      );
+    );
   }
 }
 
@@ -22,36 +30,34 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( //provides a standard page layout, AppBar, body , text etc.
-        appBar: AppBar(
-          title: const Text('Activity 1'),
-        ),
-        body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('CHRISTIAN PERALTA\n ',
-                    style: TextStyle(fontSize: 20)
-                ),
-                Image.network('https://c8.alamy.com/comp/P4JDDY/original-film-title-kimi-no-na-wa-english-title-your-name-film-director-makoto-shinkai-year-2016-credit-amusethe-answer-studiocomix-wave-filmeast-japan-marketing-album-P4JDDY.jpg',
-                  width: 1000,
-                  height: 500,
-                ),
-                const SizedBox(height:50),
-                const Text(
-                    'TITLE: YOUR NAME',
-                    style: TextStyle(fontSize: 24, fontStyle: FontStyle.italic)
-                ),
-                const Text ('\n SYNOPSIS',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)
-                    ),
-                const Text( '\n Your Name (Kimi no Na wa) is a Japanese animated romantic fantasy film directed by Makoto Shinkai. It follows two teenagers, Taki, a high school boy in Tokyo,'
-                    '\n and Mitsuha, a girl living in a rural town who mysteriously begin to swap bodies intermittently.As they navigate each others lives, they form a deep connection'
-                    '\n without ever meeting in person. However, when the body-swapping suddenly stops Taki sets out to find Mitsuha—only to uncover a shocking truth that transcends time and fate.'
-               , style: TextStyle(fontSize: 15, fontStyle: FontStyle.italic) ),
-              ]
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Home Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network('https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQKlYs1lQsPElSDl4S504hTeN8cLgcGRJRRmfqiXobsRLkzbJzhIrQNYa4p1ix4SO0DWeUI',
+              height: 600,
+              width: 300,
             ),
+            const Text(
+              'Haikyu!! The Dumpster Battle',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              '\nBoth Karasuno and Nekoma have made it to the third round of nationals.\n'
+                  'They are finally going face-to-face for the The Dumpster Battle, the much\n'
+                  'awaited face-off. Once the match commences, the two teams waste no time\n'
+                  'entering into an intense and fast-paced rally. Tension rises as both teams\n'
+                  'are determined to come out victoriously. ',
+                textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.w800),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
